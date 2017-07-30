3cb689717ec1132204c668749895a2cfab2b893c
lawrdenn <lawrdenn@outlook.com>
2017-07-30 22:27:29 +0100

    Unity game

---
+++ Game in 1 hour/Assets/Scripts/Enemy_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Script : MonoBehaviour {
+
+	private float speed;
+
+	private float transformZ;
+
+	public GameObject player;
+
+
+	void Start () {
+
+		transform.position = new Vector3 (Random.Range(-5.5f, 5.5f), 1, transform.position.z);
+
+		speed = player.GetComponent<Player_Script> ().enemySpeed;
+
+		transformZ = transform.position.z;
+	}
+	
+
+	void Update () {
+		speed = player.GetComponent<Player_Script> ().enemySpeed;
+		transform.position += Vector3.back * Time.deltaTime * 10 * speed;
+	}
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.name == "collider") {
+			transform.position = new Vector3 (Random.Range(-5.5f, 5.5f), 1, 40);
+			transform.GetChild (0).gameObject.SetActive (true);
+		}
+	}
+	public void play(){
+		
+		transform.position = new Vector3 (Random.Range (-5.5f, 5.5f), 1, transformZ);
+
+	}
+}
---
+++ Game in 1 hour/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Script : MonoBehaviour {
+
+	public float speed;
+
+	public float enemySpeed;
+
+	public int score;
+
+	public GameObject playButton;
+	public GameObject gameOverText;
+	public Text scoreText;
+	public GameObject gameName;
+
+
+	void Start () {
+		gameObject.SetActive (false);
+		gameOverText.SetActive (false);
+		scoreText.gameObject.SetActive (false);
+		gameName.SetActive (true);
+	}
+	
+
+	void Update () {
+		enemySpeed += 0.001f;
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			if (transform.position.x > -5.5f) {
+				transform.position += Vector3.left * Time.deltaTime * 10 * speed;
+			}
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			if (transform.position.x < 5.5f) {
+				transform.position += Vector3.right * Time.deltaTime * 10 * speed;
+			}
+		}
+		scoreText.text = "" + score;
+	}
+	void OnTriggerEnter (Collider col){
+		if (col.gameObject.name == "Score collider") {
+			score += 1;
+			col.gameObject.SetActive (false);
+		}
+	}
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.name == "Enemy") {
+			gameOver ();
+		}
+	}
+	public void play(){
+		gameObject.SetActive (true);
+		playButton.SetActive (false);
+		gameOverText.SetActive (false);
+		scoreText.gameObject.SetActive (true);
+		transform.position = new Vector3 (0, 0, -8);
+		score = 0;
+		gameName.SetActive (false);
+		enemySpeed = 1;
+
+	}
+	public void gameOver(){
+		gameObject.SetActive (false);
+		playButton.SetActive (true);
+		playButton.GetComponentInChildren<Text> ().text = "Replay?";
+		gameOverText.SetActive (true);
+		scoreText.gameObject.SetActive (false);
+
+	}
+}
